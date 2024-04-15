765782a3bd105c5147d7e14477e6bfbd96c173ee
chiilog <purgexpurge@gmail.com>
2024-04-15 10:22:24 +0900

    プラグインファイル用意

---
+++ sns-share-block.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Chiilog Share Button
+ * Description:       block binding api test.
+ * Requires at least: 6.5
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            mel_cha
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       chiilog-share-button
+ *
+ * @package ChiilogShareButton
+ */
+
