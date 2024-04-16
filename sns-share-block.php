cb19bb8906c67ef536f220c7ba11675dd19ef49c
chiilog <purgexpurge@gmail.com>
2024-04-16 11:58:25 +0900

    register_block_bindings_source修正

--- sns-share-block.php
+++ sns-share-block.php
@@ -16,10 +16,17 @@
 add_action( 'init', 'chiilog_register_block_bindings' );
 
 function chiilog_register_block_bindings() {
-	register_block_bindings_source( 'chiilog/share-button', array(
-		'label'              => __( 'SNS', 'chiilog-share-button' ),
+	register_block_bindings_source( 'chiilog/share-button-link', array(
+		'label'              => __( 'SNS URL', 'chiilog/share-button' ),
 		'get_value_callback' => 'chiilog_sns_data_bindings',
 		'uses_context'       => [ 'postId' ]
+	) );
+
+	register_block_bindings_source( 'chiilog/share-button-target', array(
+		'label'              => __( 'Link Target', 'chiilog/share-button' ),
+		'get_value_callback' => function () {
+			return '_blank';
+		}
 	) );
 }
 
@@ -49,28 +56,4 @@ function chiilog_sns_data_bindings( $source_args, WP_Block $block_instance ) {
 		default:
 			return null;
 	}
-}
-
-/**
- * シェアボタンの場合、ボタンのlinkTargetを_blankに変換する
- *
- * @param string $block_content
- * @param WP_Block $block
- *
- * @return string
- */
-add_filter( 'render_block_core/button', 'add_link_target_for_binding_button', 10, 2 );
-
-function add_link_target_for_binding_button( $block_content, $block ) {
-	$processor = new WP_HTML_Tag_Processor( $block_content );
-
-	if ( isset( $block['attrs']['metadata']['bindings']['url']['source'] ) ) {
-		$binding_source = $block['attrs']['metadata']['bindings']['url']['source'];
-		if ( $binding_source === 'chiilog/share-button' ) {
-			$processor->next_tag( array( 'class_name' => 'wp-block-button__link' ) );
-			$processor->set_attribute( 'target', '_blank' );
-		}
-	}
-
-	return $processor->get_updated_html();
 }
